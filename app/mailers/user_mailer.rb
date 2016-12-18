7a451b9ad5edb56644f56dc1a0986945508a5cf2
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2016-12-18 14:26:52 -0500

    style update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,38 +1,36 @@
 class UserMailer < ApplicationMailer
-	default from: 'csc444.golfi@gmail.com'
+    default from: 'csc444.golfi@gmail.com'
 
-	def signup_email(user)
-		@user = user
-		mail(to: @user.email, subject: 'Welcome to GOLFi!')
-	end
+    def signup_email(user)
+        @user = user
+        mail(to: @user.email, subject: 'Welcome to GOLFi!')
+    end
 
-	def player_email(player)
-		@player = player
-		@user = Account.find_by id: @player.account_id
-		@tournament = Tournament.find_by id: @player.tournament_id
-		mail(to: @user.email, subject: 'You have signed up for a tournament!')
-	end
+    def player_email(player)
+        @player = player
+        @user = Account.find_by id: @player.account_id
+        @tournament = Tournament.find_by id: @player.tournament_id
+        mail(to: @user.email, subject: 'You have signed up for a tournament!')
+    end
 
-	def guest_email(tournament,transaction,user)
-		@tournament = tournament
-		@transaction = transaction
-		@user = user
-		mail(to: @user.email, subject: 'Welcome to watch a tournament!')
-	end
+    def guest_email(tournament, transaction, user)
+        @tournament = tournament
+        @transaction = transaction
+        @user = user
+        mail(to: @user.email, subject: 'Welcome to watch a tournament!')
+    end
 
+    def sponsorship_email(sponsorship)
+        @sponsorship = sponsorship
+        @sponsor = Sponsor.find_by id: @sponsorship.sponsor_id
+        @user = Account.find_by id: @sponsor.account_id
+        @tournament = Tournament.find_by id: @sponsorship.tournament_id
+        mail(to: @user.email, subject: 'You have sponsored a tournament!')
+    end
 
-	def sponsorship_email(sponsorship)
-		@sponsorship = sponsorship
-		@sponsor = Sponsor.find_by id: @sponsorship.sponsor_id
-		@user = Account.find_by id: @sponsor.account_id
-		@tournament = Tournament.find_by id: @sponsorship.tournament_id
-		mail(to: @user.email, subject: 'You have sponsored a tournament!')
-	end
+    def contact_us_email(contact)
+        @contact = contact
 
-	def contact_us_email(contact)
-		@contact = contact
-
-		mail(to: 'csc444.golfi@gmail.com', subject: 'User Feedback')
-	end
-
+        mail(to: 'csc444.golfi@gmail.com', subject: 'User Feedback')
+    end
 end
